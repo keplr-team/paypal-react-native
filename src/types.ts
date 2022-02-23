28d86c8d471619ecf39201fd7c0b19657d950bc8
christophe-kepler <christophe@gokepler.fr>
2022-02-23 14:17:07 +0100

    fix: export initPaypal instead of doing it in PaypalButton

---
+++ src/types.ts
@@ -0,0 +1,24 @@
+export type PaypalConfig = {
+  clientId: string;
+  returnUrl: string;
+  live: boolean;
+};
+
+export type PaypalButtonVariant = {
+  color: 'gold' | 'silver' | 'black' | 'white' | 'blue';
+  size: 'expanded' | 'collapsed' | 'mini' | 'full';
+  label: 'payWith' | 'buyNow' | 'checkout';
+  shape: 'rounded' | 'hardEdges' | 'softEdges';
+};
+
+export type PaypalButtonShipping = {
+  address?: string;
+  city?: string;
+  postalCode?: string;
+  countryCode: string;
+};
+export type PaypalButtonOrder = {
+  referenceId: string;
+  amount: string;
+  currency: string;
+};
