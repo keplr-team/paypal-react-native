6d6f83966566021a644542f5f616e676cc94431a
Anthony Buisson <anthony.buisson@gokepler.fr>
2022-10-18 14:34:06 +0200

    feat: add expo plugin

---
+++ plugin/src/withPaypal.ts
@@ -0,0 +1,202 @@
+/**
+ * Ce plugin normalement devrait être dans la lib RN paypal mais pour faciliter le test du plugin je l'ai mis pour le moment dans le projet de
+ * l'app de test.
+ */
+import {
+  AndroidConfig,
+  ConfigPlugin,
+  createRunOncePlugin,
+  withProjectBuildGradle,
+  withAndroidManifest,
+  withMainApplication,
+  withAppBuildGradle,
+} from '@expo/config-plugins';
+import {
+  createGeneratedHeaderComment,
+  MergeResults,
+  removeGeneratedContents,
+} from '@expo/config-plugins/build/utils/generateCode';
+
+const pkg = require('../../package.json');
+
+export type Props = {
+  paypalClientId: string;
+  paypalReturnUrl: string;
+  env: string
+};
+
+/**
+ * Setup Paypal in the onCreate method of the MainApplication class.
+ */
+const withPaypalMainApplication: ConfigPlugin<Props> = (
+  config,
+  { paypalClientId, paypalReturnUrl, env },
+) => {
+  return withMainApplication(config, async (config) => {
+    config.modResults.contents = modifyMainApplication({
+      contents: config.modResults.contents,
+      paypalClientId,
+      paypalReturnUrl,
+      env,
+      packageName: AndroidConfig.Package.getPackage(config),
+    });
+
+    return config;
+  });
+};
+
+const modifyMainApplication = ({
+  contents,
+  paypalClientId,
+  paypalReturnUrl,
+  env,
+  packageName,
+}: {
+  contents: string;
+  paypalClientId: string;
+  paypalReturnUrl: string;
+  env: string;
+  packageName: string | null;
+}) => {
+  if (!packageName) {
+    throw new Error("Android package not found");
+  }
+
+  // Add the import line to the top of the file
+  const importLine = `import com.paypalreactnative.RNPaypalModule;`;
+  if (!contents.includes(importLine)) {
+    const packageImport = `package ${packageName};`;
+    contents = contents.replace(`${packageImport}`, `${packageImport}\n${importLine}`);
+  }
+
+  // Add the init line in the onCreate method
+  const initLine = `RNPaypalModule.Companion.setup(this, "${paypalClientId}", "${paypalReturnUrl}", "${env}");`;
+  if (!contents.includes(initLine)) {
+    const soLoaderLine = `SoLoader.init(this, /* native exopackage */ false);`;
+    contents = contents.replace(`${soLoaderLine}`, `${soLoaderLine}\n\t\t${initLine}\n`);
+  }
+
+  return contents;
+};
+
+/**
+ * Set tools:replace="android:allowBackup" in AndroidManifest.xml
+ */
+const withPaypalAndroidManifest: ConfigPlugin<{}> = (
+  config
+) => {
+  return withAndroidManifest(config, async (config) => {
+      config.modResults.manifest.$ = {
+        ...config.modResults.manifest.$,
+        "xmlns:tools": "http://schemas.android.com/tools",
+      };
+
+      if (config.modResults.manifest.application?.[0]) {
+        config.modResults.manifest.application[0].$ = {
+          ...config.modResults.manifest.application[0].$,
+          "tools:replace": "android:allowBackup",
+        };
+      }
+
+
+    return config;
+  });
+};
+
+/**
+ * Add required repository to android/build.gradle
+ */
+const withPaypalProjectBuildGradle: ConfigPlugin<{}> = (config) => {
+  return withProjectBuildGradle(config, async (config) => {
+    if (config.modResults.language === 'groovy') {
+      config.modResults.contents = addPaypalRepo(config.modResults.contents).contents;
+    } else {
+      throw new Error('Cannot add paypal repo because the build.gradle is not groovy');
+    }
+    return config;
+  });
+};
+
+//   allprojects {
+//   repositories {
+//     maven {
+//       url "https://cardinalcommerceprod.jfrog.io/artifactory/android"
+//       credentials {
+//         // Be sure to add these non-sensitive credentials in order to retrieve dependencies from
+//         // the private repository.
+//         username "paypal_sgerritz"
+//         password "AKCp8jQ8tAahqpT5JjZ4FRP2mW7GMoFZ674kGqHmupTesKeAY2G8NcmPKLuTxTGkKjDLRzDUQ"
+//       }
+//     }
+//   }
+// }
+const gradleMaven = `allprojects {\n\trepositories {\n\t\tmaven {\n\t\t\turl "https://cardinalcommerceprod.jfrog.io/artifactory/android"\n\t\t\tcredentials {\n\t\t\t\tusername "paypal_sgerritz"\n\t\t\t\tpassword "AKCp8jQ8tAahqpT5JjZ4FRP2mW7GMoFZ674kGqHmupTesKeAY2G8NcmPKLuTxTGkKjDLRzDUQ"\n\t\t\t}\n\t\t}\n\t}\n}`;
+
+export function addPaypalRepo(src: string): MergeResults {
+  return appendContents({
+    tag: 'expo-paypal-repo',
+    src,
+    newSrc: gradleMaven,
+    comment: '//',
+  });
+}
+
+// Fork of config-plugins mergeContents, but appends the contents to the end of the file.
+function appendContents({
+  src,
+  newSrc,
+  tag,
+  comment,
+}: {
+  src: string;
+  newSrc: string;
+  tag: string;
+  comment: string;
+}): MergeResults {
+  const header = createGeneratedHeaderComment(newSrc, tag, comment);
+  if (!src.includes(header)) {
+    // Ensure the old generated contents are removed.
+    const sanitizedTarget = removeGeneratedContents(src, tag);
+    const contentsToAdd = [
+      // @something
+      header,
+      // contents
+      newSrc,
+      // @end
+      `${comment} @generated end ${tag}`,
+    ].join('\n');
+
+    return {
+      contents: sanitizedTarget ?? src + contentsToAdd,
+      didMerge: true,
+      didClear: !!sanitizedTarget,
+    };
+  }
+  return { contents: src, didClear: false, didMerge: false };
+}
+
+/**
+ * Configure android files to add paypal
+ */
+const withPaypalAndroid: ConfigPlugin<Props> = (
+  config,
+  { paypalClientId, paypalReturnUrl, env },
+) => {
+  config = withPaypalProjectBuildGradle(config, {});
+  config = withPaypalAndroidManifest(config, {});
+  config = withPaypalMainApplication(config, {
+    paypalClientId,
+    paypalReturnUrl,
+    env,
+  });
+
+  return config;
+};
+
+const withPaypal: ConfigPlugin<Props> = (config, props) => {
+  config = withPaypalAndroid(config, props);
+
+  return config;
+};
+
+export default createRunOncePlugin(withPaypal, pkg.name, pkg.version);
